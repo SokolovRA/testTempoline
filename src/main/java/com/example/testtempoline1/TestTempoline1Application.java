11c869feb9548d323584d5c75fb8757d2883d737
Sokolov Roman <sokolov_r.a@bk.ru>
2023-09-22 11:24:09 +0300

    start project

---
+++ src/main/java/com/example/testtempoline1/TestTempoline1Application.java
@@ -0,0 +1,13 @@
+package com.example.testtempoline1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTempoline1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTempoline1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testtempoline1/TestTempoline1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtempoline1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTempoline1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
